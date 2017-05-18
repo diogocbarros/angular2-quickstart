5b92609d928156204692081dd740ac96459dc4d9
Diogo Barros <diogocbarros@hotmail.com>
2017-05-18 08:19:34 -0400

    first commit

---
+++ 01-cadastro-contatos/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+   selector: 'my-app',
+   template: '<h1>My first Angular app<h1>'
+})
+export class appComponent{
+
+}
